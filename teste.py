33639ed886b0602c202aea54268eafd277341412
Gabriela <130176270+GabrielaSchubert@users.noreply.github.com>
2023-05-01 21:30:37 -0300

    Update teste.py

--- teste.py
+++ teste.py
@@ -7,11 +7,15 @@ num2 = 3
 num_resultado = num1+num2
 print("o resultado", num_resultado)
 
+Condicoes
+
 idade = int(input("digite a idade do carro: "))
 if idade <= 3:
  print("seu carro e novo")
 else:
   print("seu carro e velho")
+  
+Repeticoes
 
 fim = int(input("digite o último número a ser imprimido: "))
 x = 1
