7a75b27203b7dad244ac8da5d0b5160e55ae7faf
Gabriela <130176270+GabrielaSchubert@users.noreply.github.com>
2023-05-01 21:29:02 -0300

    Add files via upload

--- teste.py
+++ teste.py
@@ -6,3 +6,15 @@ num1 = 5
 num2 = 3
 num_resultado = num1+num2
 print("o resultado", num_resultado)
+
+idade = int(input("digite a idade do carro: "))
+if idade <= 3:
+ print("seu carro e novo")
+else:
+  print("seu carro e velho")
+
+fim = int(input("digite o último número a ser imprimido: "))
+x = 1
+while x <= fim:
+  print(x)
+  x = x + 1
