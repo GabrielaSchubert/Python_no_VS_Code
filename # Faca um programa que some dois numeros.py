1b3a899ef5d6cf263f5869480ff96d33fe716a50
Gabriela <130176270+GabrielaSchubert@users.noreply.github.com>
2023-06-28 21:56:32 -0300

    Delete # Faca um programa que some dois numeros.py

--- # Faca um programa que some dois numeros.py
+++
@@ -1,6 +0,0 @@
-# Faca um programa que some dois numeros 
-
-n1 = int(input("digite um numero: "))
-n2 = int(input("digite outro numero: "))
-resultado = n1+n2
-print("a soma dos dois numeros", resultado )
