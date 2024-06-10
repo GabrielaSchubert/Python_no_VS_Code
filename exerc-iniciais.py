3ccbf2786064d39794a5312166b9d5f85ab2c44f
Gabriela Schubert <gabrielaschubert172@gmail.com>
2024-06-10 19:04:56 -0300

    exercícios iniciais

---
+++ exerc-iniciais.py
@@ -0,0 +1,26 @@
+# 1- Variáveis e tipos de dados
+nome = "Seunome"
+idade = 20
+print(f"Olá, {nome}! Você tem {idade} anos")
+
+# 2- Estruturas condicionais
+nota = 8
+if nota >= 7:
+    print("Aprovado!")
+else: 
+    print("Reprovado!")
+
+# 3- Estruturas de repetição
+for num in range(1,11):
+    print(num)
+
+# 4- Funções
+def soma(a,b):
+    return a + b
+resultado = soma(5,10)
+print("O resultado da soma é:", resultado)
+
+# 5- Lista
+numeros = [2,4,6,8,10]
+soma = sum(numeros)
+print("O resultado da soma dos números da lista é:", soma) 
